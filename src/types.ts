1be71f4e51334abd691f1f1f159975a33ef751f9
ghostebony <ghostebony@proton.me>
2022-08-10 14:53:21 -0300

    fix http RequestOptions type

--- src/types.ts
+++ src/types.ts
@@ -1,4 +1,5 @@
-export interface RequestOptions extends RequestInit {
+export interface RequestOptions extends Omit<RequestInit, "body"> {
+	body?: Record<string, any> | BodyInit | null;
 	cookies?: Record<string, string>;
 	params?: Record<string | number, string | number | boolean>;
 	response?: "arrayBuffer" | "blob" | "formData" | "json" | "text" | "none";
