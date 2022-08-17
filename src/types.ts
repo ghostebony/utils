c8dc588ebeb062f3e55381fe912811516cf91843
ghostebony <pedroamericolouzada@uol.com.br>
2022-08-17 17:25:24 -0300

    add <UnionToIntersection> type

--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,9 @@
+export type UnionToIntersection<U> = (U extends any ? (k: U) => void : never) extends (
+	k: infer I
+) => void
+	? I
+	: never;
+
 export interface RequestOptions extends Omit<RequestInit, "body"> {
 	body?: Record<string, any> | BodyInit | null;
 	cookies?: Record<string, string>;
